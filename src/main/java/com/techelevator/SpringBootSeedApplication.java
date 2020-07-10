d85647a8dbb74ee2e154a2826b23c732ce3b9328
Tom Medvitz <tom@techelevator.com>
2020-07-10 09:42:50 -0400

    Initial commit

---
+++ src/main/java/com/techelevator/SpringBootSeedApplication.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSeedApplication.class, args);
+	}
+
+}
